1a24100c03ec8b97f9203ddd354a410967a7567e
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-10-19 22:20:06 -0700

    Update discord.js

--- discord.js
+++ discord.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const redirect = encodeURIComponent('https://absolute-music.github.io/api/discord/callback');
+const redirect = encodeURIComponent('https://absolute-music.github.io/loggedin.html/api/discord/callback');
 
 router.get('/login', (req, res) => {
-  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=633176467068551168&redirect_uri=https%3A%2F%2Fabsolute-music.github.io%2Fapi%2Fdiscord%2Fcallback&response_type=code&scope=identify`);
+  res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=633176467068551168&redirect_uri=https%3A%2F%2Fabsolute-music.github.io%2Floggedin.html%2Fapi%2Fdiscord%2Fcallback&response_type=code&scope=identify`);
 });
 
 module.exports = router;
